40ff6e1243989f3d213e92fe7990ea3b3740786c
Leonardo Lima <leonardolima1356@gmail.com>
2025-04-07 16:07:01 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import style from './App.module.css'
+import { Menu } from './components/menu'
+
+function App() {
+  return (
+    <>
+      <Menu option01='Sessão 01' option02='Sessão 02' option03='contato'/>
+      <main>
+        <section id='s1'>
+          <h2>sessao 1</h2>
+        </section>
+        <section id='s2'>
+          <h2>sessao 2</h2>
+        </section>
+        <section id='s3'>
+          <h2>sessao 3</h2>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
