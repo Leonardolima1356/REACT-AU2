a8da3659cae04eb4cf73d263cd0f889c90ecdf1b
Leonardo Lima <leonardolima1356@gmail.com>
2025-04-13 16:58:22 -0300

    Add files via upload

--- src/assets/mock/projeto.js
+++ src/assets/mock/projeto.js
@@ -2,7 +2,7 @@ import github from '../images/github.png'
 
 export const projetos = [
     {
-        text: "Atividades HTML", img: github, link: "https://github.com/Leonardolima1356/TDS-148/tree/main/Javascript-inicial!"
+        text: "Atividades HTML", img: github, link: "https://github.com/Leonardolima1356/TDS-148/tree/main"
     },
     {
         text: "Site da Barbie", img: github, link: "https://github.com/Leonardolima1356/TDS-148/tree/main/Javascript-inicial/Barbie"
